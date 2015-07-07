8d3b5af183025692c9c3e16ffe7c75124e99dafa
Samuel A. Falvo II <sam.falvo@rackspace.com>
2015-07-07 14:08:06 -0700

    OMG.  I can't believe I'm making these errors today.

--- attr/validators.py
+++ attr/validators.py
@@ -103,7 +103,7 @@ class _OptionalValidator(object):
     def __repr__(self):
         return (
             "<instance_of validator for type {type!r} or None>"
-            .format(type=type(v))
+            .format(type=type(self.v))
         )
 
 
