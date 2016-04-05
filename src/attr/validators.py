402b8255392a841e9ae81e4df4467e174c90a06b
Hynek Schlawack <hs@ox.cx>
2016-04-05 10:48:17 +0200

    Clarify instance_of validator also takes tuples

--- src/attr/validators.py
+++ src/attr/validators.py
@@ -35,13 +35,13 @@ def instance_of(type):
     """
     A validator that raises a :exc:`TypeError` if the initializer is called
     with a wrong type for this particular attribute (checks are perfomed using
-    :func:`isinstance`).
+    :func:`isinstance` therefore it's also valid to pass a tuple of types).
 
     :param type: The type to check for.
-    :type type: type
+    :type type: type or tuple of types
 
     The :exc:`TypeError` is raised with a human readable error message, the
-    attribute (of type :class:`attr.Attribute`), the expected type and the
+    attribute (of type :class:`attr.Attribute`), the expected type, and the
     value it got.
     """
     return _InstanceOfValidator(type)
