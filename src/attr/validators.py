80bfd33b1b77a7fb68a18ab528e6bf16f42be77c
Sylvain Marié <sylvain.marie@schneider-electric.com>
2017-03-02 13:25:34 +0100

    from parsyfiles development (https://github.com/smarie/python-simple-file-collection-parsing-framework): * added new 'chain' validator to allow users to chain their custom validators with, for example, 'instance_of' * added utility methods to get the declared attribute type (by inspecting its validators) and to check if it is optional

--- src/attr/validators.py
+++ src/attr/validators.py
@@ -112,3 +112,79 @@ def optional(validator):
     :param validator: A validator that is used for non-``None`` values.
     """
     return _OptionalValidator(validator)
+
+
+@attr.s(repr=False, slots=True)
+class _AndValidator(object):
+    validators = attr.ib()
+
+    def __call__(self, inst, attr, value):
+        for v in self.validators:
+            v(inst, attr, value)
+        return
+
+    def __repr__(self):
+        return (
+            "<validator sequence : {seq}>".format(seq=repr(self.validators))
+        )
+
+
+def chain(*validators):
+    """
+    A validator that applies several validators in order
+
+    :param validators: A sequence of validators
+    """
+    return _AndValidator(validators)
+
+
+def _guess_type_from_validator(validator):
+    """
+    Utility method to return the declared type of an attribute or None. It handles _OptionalValidator and _AndValidator
+    in order to unpack the validators.
+
+    :param validator:
+    :return: the type of attribute declared in an inner 'instance_of' validator (if any is found, the first one is used)
+    or None if no inner 'instance_of' validator is found
+    """
+    if isinstance(validator, _OptionalValidator):
+        # Optional : look inside
+        return _guess_type_from_validator(validator)
+
+    elif isinstance(validator, _AndValidator):
+        # Sequence : try each of them
+        for v in validator.validators:
+            typ = _guess_type_from_validator(v)
+            if typ is not None:
+                return typ
+        return None
+
+    elif isinstance(validator, _InstanceOfValidator):
+        # InstanceOf validator : found it !
+        return validator.type
+
+    else:
+        # we could not find the type
+        return None
+
+
+def guess_type_from_validators(attr):
+    """
+    Utility method to return the declared type of an attribute or None. It handles _OptionalValidator and _AndValidator
+    in order to unpack the validators.
+
+    :param attr:
+    :return: the type of attribute declared in an inner 'instance_of' validator (if any is found, the first one is used)
+    or None if no inner 'instance_of' validator is found
+    """
+    return _guess_type_from_validator(attr.validator)
+
+
+def is_mandatory(attr):
+    """
+    Helper method to find if an attribute is mandatory, by checking if its validator is 'optional' or not.
+
+    :param attr:
+    :return:
+    """
+    return not isinstance(attr.validator, _OptionalValidator)
