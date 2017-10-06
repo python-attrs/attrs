4a19d9b14d4df0cd881b3698407c8f2a6e04842b
Hynek Schlawack <hs@ox.cx>
2017-10-06 14:38:23 +0200

    Add safe-guard against collecting too much

--- tests/test_annotations.py
+++ tests/test_annotations.py
@@ -2,8 +2,6 @@
 Tests for PEP-526 type annotations.
 """
 
-from __future__ import absolute_import, division, print_function
-
 import pytest
 
 import attr
@@ -11,7 +9,7 @@ import attr
 import typing
 
 
-class TestAnnotations(object):
+class TestAnnotations:
     """
     Tests for types derived from variable annotations (PEP-526).
     """
@@ -21,7 +19,7 @@ class TestAnnotations(object):
         Sets the `Attribute.type` attr from basic type annotations.
         """
         @attr.s
-        class C(object):
+        class C:
             x: int = attr.ib()
             y = attr.ib(type=str)
             z = attr.ib()
@@ -32,24 +30,36 @@ class TestAnnotations(object):
 
     def test_catches_basic_type_conflict(self):
         """
-        Raises ValueError type is specified both ways.
+        Raises ValueError if type is specified both ways.
         """
         with pytest.raises(ValueError) as e:
             @attr.s
             class C:
                 x: int = attr.ib(type=int)
 
-        assert ("Type annotation and type argument cannot "
-                "both be present",) == e.value.args
+        assert (
+            "Type annotation and type argument cannot both be present",
+        ) == e.value.args
 
     def test_typing_annotations(self):
         """
         Sets the `Attribute.type` attr from typing annotations.
         """
         @attr.s
-        class C(object):
+        class C:
             x: typing.List[int] = attr.ib()
             y = attr.ib(type=typing.Optional[str])
 
         assert typing.List[int] is attr.fields(C).x.type
         assert typing.Optional[str] is attr.fields(C).y.type
+
+    def test_only_attrs_annotations_collected(self):
+        """
+        Annotations that aren't set to an attr.ib are ignored.
+        """
+        @attr.s
+        class C:
+            x: typing.List[int] = attr.ib()
+            y: int
+
+        assert 1 == len(attr.fields(C))
