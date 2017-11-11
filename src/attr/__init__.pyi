f254513897d8cf40b65c7f323ce8cdb34f1bd30e
Chad Dombrova <chadrik@gmail.com>
2017-11-11 14:47:01 -0800

    Improve overloads using PyCharm order-based approach
    overloads are pretty broken in mypy.  the best we can do for now is target PyCharm, which is much more forgiving.

--- src/attr/__init__.pyi
+++ src/attr/__init__.pyi
@@ -12,8 +12,8 @@ _C = TypeVar('_C', bound=type)
 _M = TypeVar('_M', bound=Mapping)
 _I = TypeVar('_I', bound=Iterable)
 
-_ValidatorType = Callable[[Any, 'Attribute', Any], Any]
-_ConverterType = Callable[[Any], Any]
+_ValidatorType = Callable[[Any, 'Attribute', _T], Any]
+_ConverterType = Callable[[Any], _T]
 _FilterType = Callable[['Attribute', Any], bool]
 
 # _make --
@@ -23,28 +23,22 @@ NOTHING : object
 # Factory lies about its return type to make this possible: `x: List[int] = Factory(list)`
 def Factory(factory: Union[Callable[[], _T], Callable[[Any], _T]], takes_self: bool = ...) -> _T: ...
 
-class Attribute:
+class Attribute(Generic[_T]):
     __slots__ = ("name", "default", "validator", "repr", "cmp", "hash", "init", "convert", "metadata", "type")
     name: str
     default: Any
-    validator: Optional[Union[_ValidatorType, List[_ValidatorType], Tuple[_ValidatorType, ...]]]
+    validator: Optional[Union[_ValidatorType[_T], List[_ValidatorType[_T]], Tuple[_ValidatorType[_T], ...]]]
     repr: bool
     cmp: bool
     hash: Optional[bool]
     init: bool
-    convert: Optional[_ConverterType]
+    convert: Optional[_ConverterType[_T]]
     metadata: Mapping
-    type: Optional[Any]
+    type: Optional[Type[_T]]
 
 
-# order here matters:  if default is provided but not type, we want the first overload chosen so that the type is based on default
-@overload
-def attr(default: _T = ..., validator: Optional[Union[_ValidatorType, List[_ValidatorType], Tuple[_ValidatorType, ...]]] = ..., repr: bool = ..., cmp: bool = ..., hash: Optional[bool] = ..., init: bool = ..., convert: Optional[_ConverterType] = ..., metadata: Mapping = ..., type: Type[_T] = ...) -> _T: ...
-# NOTE: this overload for `attr` returns Any so that static analysis passes when used in the form:  x : int = attr()
-@overload
-def attr(default: _T = ..., validator: Optional[Union[_ValidatorType, List[_ValidatorType], Tuple[_ValidatorType, ...]]] = ..., repr: bool = ..., cmp: bool = ..., hash: Optional[bool] = ..., init: bool = ..., convert: Optional[_ConverterType] = ..., metadata: Mapping = ...) -> Any: ...
+def attr(default: _T = ..., validator: Optional[Union[_ValidatorType[_T], List[_ValidatorType[_T]], Tuple[_ValidatorType[_T], ...]]] = ..., repr: bool = ..., cmp: bool = ..., hash: Optional[bool] = ..., init: bool = ..., convert: Optional[_ConverterType[_T]] = ..., metadata: Mapping = ..., type: Type[_T] = ...) -> _T: ...
 
-# we use Any instead of _CountingAttr so that e.g. `make_class('Foo', [attr.ib()])` is valid
 
 @overload
 def attributes(maybe_cls: _C = ..., these: Optional[Dict[str, Any]] = ..., repr_ns: Optional[str] = ..., repr: bool = ..., cmp: bool = ..., hash: Optional[bool] = ..., init: bool = ..., slots: bool = ..., frozen: bool = ..., str: bool = ...) -> _C: ...
@@ -54,15 +48,21 @@ def attributes(maybe_cls: None = ..., these: Optional[Dict[str, Any]] = ..., rep
 def fields(cls: type) -> Tuple[Attribute, ...]: ...
 def validate(inst: Any) -> None: ...
 
+# we use Any instead of _CountingAttr so that e.g. `make_class('Foo', [attr.ib()])` is valid
 def make_class(name, attrs: Union[List[Any], Dict[str, Any]], bases: Tuple[type, ...] = ..., **attributes_arguments) -> type: ...
 
 # _funcs --
 
-# FIXME: having problems assigning a default to the factory typevars
-# def asdict(inst: Any, recurse: bool = ..., filter: Optional[_FilterType] = ..., dict_factory: Type[_M] = dict, retain_collection_types: bool = ...) -> _M[str, Any]: ...
-# def astuple(inst: Any, recurse: bool = ..., filter: Optional[_FilterType] = ..., tuple_factory: Type[_I] = tuple, retain_collection_types: bool = ...) -> _I: ...
-def asdict(inst: Any, recurse: bool = ..., filter: Optional[_FilterType] = ..., dict_factory: Type[_M] = ..., retain_collection_types: bool = ...) -> _M: ...
-def astuple(inst: Any, recurse: bool = ..., filter: Optional[_FilterType] = ..., tuple_factory: Type[_I] = ..., retain_collection_types: bool = ...) -> _I: ...
+@overload
+def asdict(inst: Any, *, recurse: bool = ..., filter: Optional[_FilterType] = ..., dict_factory: Type[_M], retain_collection_types: bool = ...) -> _M: ...
+@overload
+def asdict(inst: Any, *, recurse: bool = ..., filter: Optional[_FilterType] = ..., retain_collection_types: bool = ...) -> Dict[str, Any]: ...
+
+@overload
+def astuple(inst: Any, *, recurse: bool = ..., filter: Optional[_FilterType] = ..., tuple_factory: Type[_I], retain_collection_types: bool = ...) -> _I: ...
+@overload
+def astuple(inst: Any, *, recurse: bool = ..., filter: Optional[_FilterType] = ..., retain_collection_types: bool = ...) -> tuple: ...
+
 def has(cls: type) -> bool: ...
 def assoc(inst: _T, **changes) -> _T: ...
 def evolve(inst: _T, **changes) -> _T: ...
