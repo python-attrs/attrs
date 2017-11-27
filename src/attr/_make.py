7a2f742a6d7e2218fd8f42501b85dd502e3c8deb
Yaroslav Halchenko <debian@onerussian.com>
2017-11-27 14:54:59 -0500

    BF: reverse MRO as well if we are reversing the attrs at the end

--- src/attr/_make.py
+++ src/attr/_make.py
@@ -279,7 +279,7 @@ def _transform_attrs(cls, these, auto_attribs):
     # redefinitions deeper in the hierarchy.
     super_attrs = []
     taken_attr_names = {a.name: a for a in non_super_attrs}
-    for super_cls in cls.__mro__[1:-1]:
+    for super_cls in reversed(cls.__mro__[1:-1]):
         sub_attrs = getattr(super_cls, "__attrs_attrs__", None)
         if sub_attrs is not None:
             # We iterate over sub_attrs backwards so we can reverse the whole
@@ -291,7 +291,7 @@ def _transform_attrs(cls, these, auto_attribs):
                     super_attrs.append(a)
                     taken_attr_names[a.name] = a
                 elif prev_a == a:
-                    # This happens thru multiple inheritance.  We don't want
+                    # This happens through multiple inheritance.  We don't want
                     # to favor attributes that are further down in the tree
                     # so we move them to the back.
                     super_attrs.remove(a)
