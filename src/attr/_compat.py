dc6040f9d531475d9f03f85caa792a7536887d49
Hynek Schlawack <hs@ox.cx>
2018-01-27 21:07:14 +0100

    Make cell setting completely best effort (#334)
    * Make cell setting completely best effort
    
    We just try, catch any exception and warn if it doesn't work on a Python 3.
    Should cover edge cases such as Jython or GAE.
    
    Fixes #321
    
    * Add news fragment
    
    * Typing is hard yo

--- src/attr/_compat.py
+++ src/attr/_compat.py
@@ -87,15 +87,12 @@ else:
         return types.MappingProxyType(dict(d))
 
 
-def import_ctypes():  # pragma: nocover
+def import_ctypes():
     """
     Moved into a function for testability.
     """
-    try:
-        import ctypes
-        return ctypes
-    except ImportError:
-        return None
+    import ctypes
+    return ctypes
 
 
 if not PY2:
@@ -126,12 +123,15 @@ def make_set_closure_cell():
         def set_closure_cell(cell, value):
             cell.__setstate__((value,))
     else:
-        ctypes = import_ctypes()
-        if ctypes is not None:
+        try:
+            ctypes = import_ctypes()
+
             set_closure_cell = ctypes.pythonapi.PyCell_Set
             set_closure_cell.argtypes = (ctypes.py_object, ctypes.py_object)
             set_closure_cell.restype = ctypes.c_int
-        else:
+        except Exception:
+            # We try best effort to set the cell, but sometimes it's not
+            # possible.  For example on Jython or on GAE.
             set_closure_cell = just_warn
     return set_closure_cell
 
