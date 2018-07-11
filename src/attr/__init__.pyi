2b19c364a1929a38457a9060e3d21767c3fb24c5
Chad Dombrova <chadrik@gmail.com>
2018-07-10 22:00:41 -0700

    get pre-commit passing

--- src/attr/__init__.pyi
+++ src/attr/__init__.pyi
@@ -1,12 +1,27 @@
-from typing import Any, Callable, Dict, Generic, List, Optional, Sequence, Mapping, Tuple, Type, TypeVar, Union, overload
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Generic,
+    List,
+    Optional,
+    Sequence,
+    Mapping,
+    Tuple,
+    Type,
+    TypeVar,
+    Union,
+    overload,
+)
+
 # `import X as X` is required to make these public
 from . import exceptions as exceptions
 from . import filters as filters
 from . import converters as converters
 from . import validators as validators
 
-_T = TypeVar('_T')
-_C = TypeVar('_C', bound=type)
+_T = TypeVar("_T")
+_C = TypeVar("_C", bound=type)
 
 _ValidatorType = Callable[[Any, Attribute, _T], Any]
 _ConverterType = Callable[[Any], _T]
@@ -25,7 +40,10 @@ NOTHING: object
 @overload
 def Factory(factory: Callable[[], _T]) -> _T: ...
 @overload
-def Factory(factory: Union[Callable[[Any], _T], Callable[[], _T]], takes_self: bool = ...) -> _T: ...
+def Factory(
+    factory: Union[Callable[[Any], _T], Callable[[], _T]],
+    takes_self: bool = ...,
+) -> _T: ...
 
 class Attribute(Generic[_T]):
     name: str
@@ -42,7 +60,6 @@ class Attribute(Generic[_T]):
     def __le__(self, x: Attribute) -> bool: ...
     def __gt__(self, x: Attribute) -> bool: ...
     def __ge__(self, x: Attribute) -> bool: ...
-
 
 # NOTE: We had several choices for the annotation to use for type arg:
 # 1) Type[_T]
@@ -70,87 +87,95 @@ class Attribute(Generic[_T]):
 #
 # This form catches explicit None or no default but with no other arguments returns Any.
 @overload
-def attrib(default: None = ...,
-           validator: None = ...,
-           repr: bool = ...,
-           cmp: bool = ...,
-           hash: Optional[bool] = ...,
-           init: bool = ...,
-           convert: None = ...,
-           metadata: Optional[Mapping[Any, Any]] = ...,
-           type: None = ...,
-           converter: None = ...,
-           factory: None = ...,
-           ) -> Any: ...
+def attrib(
+    default: None = ...,
+    validator: None = ...,
+    repr: bool = ...,
+    cmp: bool = ...,
+    hash: Optional[bool] = ...,
+    init: bool = ...,
+    convert: None = ...,
+    metadata: Optional[Mapping[Any, Any]] = ...,
+    type: None = ...,
+    converter: None = ...,
+    factory: None = ...,
+) -> Any: ...
+
 # This form catches an explicit None or no default and infers the type from the other arguments.
 @overload
-def attrib(default: None = ...,
-           validator: Optional[_ValidatorArgType[_T]] = ...,
-           repr: bool = ...,
-           cmp: bool = ...,
-           hash: Optional[bool] = ...,
-           init: bool = ...,
-           convert: Optional[_ConverterType[_T]] = ...,
-           metadata: Optional[Mapping[Any, Any]] = ...,
-           type: Optional[Type[_T]] = ...,
-           converter: Optional[_ConverterType[_T]] = ...,
-           factory: Optional[Callable[[], _T]] = ...,
-           ) -> _T: ...
+def attrib(
+    default: None = ...,
+    validator: Optional[_ValidatorArgType[_T]] = ...,
+    repr: bool = ...,
+    cmp: bool = ...,
+    hash: Optional[bool] = ...,
+    init: bool = ...,
+    convert: Optional[_ConverterType[_T]] = ...,
+    metadata: Optional[Mapping[Any, Any]] = ...,
+    type: Optional[Type[_T]] = ...,
+    converter: Optional[_ConverterType[_T]] = ...,
+    factory: Optional[Callable[[], _T]] = ...,
+) -> _T: ...
+
 # This form catches an explicit default argument.
 @overload
-def attrib(default: _T,
-           validator: Optional[_ValidatorArgType[_T]] = ...,
-           repr: bool = ...,
-           cmp: bool = ...,
-           hash: Optional[bool] = ...,
-           init: bool = ...,
-           convert: Optional[_ConverterType[_T]] = ...,
-           metadata: Optional[Mapping[Any, Any]] = ...,
-           type: Optional[Type[_T]] = ...,
-           converter: Optional[_ConverterType[_T]] = ...,
-           factory: Optional[Callable[[], _T]] = ...,
-           ) -> _T: ...
+def attrib(
+    default: _T,
+    validator: Optional[_ValidatorArgType[_T]] = ...,
+    repr: bool = ...,
+    cmp: bool = ...,
+    hash: Optional[bool] = ...,
+    init: bool = ...,
+    convert: Optional[_ConverterType[_T]] = ...,
+    metadata: Optional[Mapping[Any, Any]] = ...,
+    type: Optional[Type[_T]] = ...,
+    converter: Optional[_ConverterType[_T]] = ...,
+    factory: Optional[Callable[[], _T]] = ...,
+) -> _T: ...
+
 # This form covers type=non-Type: e.g. forward references (str), Any
 @overload
-def attrib(default: Optional[_T] = ...,
-           validator: Optional[_ValidatorArgType[_T]] = ...,
-           repr: bool = ...,
-           cmp: bool = ...,
-           hash: Optional[bool] = ...,
-           init: bool = ...,
-           convert: Optional[_ConverterType[_T]] = ...,
-           metadata: Optional[Mapping[Any, Any]] = ...,
-           type: object = ...,
-           converter: Optional[_ConverterType[_T]] = ...,
-           factory: Optional[Callable[[], _T]] = ...,
-           ) -> Any: ...
-
-
+def attrib(
+    default: Optional[_T] = ...,
+    validator: Optional[_ValidatorArgType[_T]] = ...,
+    repr: bool = ...,
+    cmp: bool = ...,
+    hash: Optional[bool] = ...,
+    init: bool = ...,
+    convert: Optional[_ConverterType[_T]] = ...,
+    metadata: Optional[Mapping[Any, Any]] = ...,
+    type: object = ...,
+    converter: Optional[_ConverterType[_T]] = ...,
+    factory: Optional[Callable[[], _T]] = ...,
+) -> Any: ...
 @overload
-def attrs(maybe_cls: _C,
-          these: Optional[Dict[str, Any]] = ...,
-          repr_ns: Optional[str] = ...,
-          repr: bool = ...,
-          cmp: bool = ...,
-          hash: Optional[bool] = ...,
-          init: bool = ...,
-          slots: bool = ...,
-          frozen: bool = ...,
-          str: bool = ...,
-          auto_attribs: bool = ...) -> _C: ...
+def attrs(
+    maybe_cls: _C,
+    these: Optional[Dict[str, Any]] = ...,
+    repr_ns: Optional[str] = ...,
+    repr: bool = ...,
+    cmp: bool = ...,
+    hash: Optional[bool] = ...,
+    init: bool = ...,
+    slots: bool = ...,
+    frozen: bool = ...,
+    str: bool = ...,
+    auto_attribs: bool = ...,
+) -> _C: ...
 @overload
-def attrs(maybe_cls: None = ...,
-          these: Optional[Dict[str, Any]] = ...,
-          repr_ns: Optional[str] = ...,
-          repr: bool = ...,
-          cmp: bool = ...,
-          hash: Optional[bool] = ...,
-          init: bool = ...,
-          slots: bool = ...,
-          frozen: bool = ...,
-          str: bool = ...,
-          auto_attribs: bool = ...) -> Callable[[_C], _C]: ...
-
+def attrs(
+    maybe_cls: None = ...,
+    these: Optional[Dict[str, Any]] = ...,
+    repr_ns: Optional[str] = ...,
+    repr: bool = ...,
+    cmp: bool = ...,
+    hash: Optional[bool] = ...,
+    init: bool = ...,
+    slots: bool = ...,
+    frozen: bool = ...,
+    str: bool = ...,
+    auto_attribs: bool = ...,
+) -> Callable[[_C], _C]: ...
 
 # TODO: add support for returning NamedTuple from the mypy plugin
 class _Fields(Tuple[Attribute, ...]):
@@ -162,18 +187,20 @@ def validate(inst: Any) -> None: ...
 
 # TODO: add support for returning a proper attrs class from the mypy plugin
 # we use Any instead of _CountingAttr so that e.g. `make_class('Foo', [attr.ib()])` is valid
-def make_class(name: str,
-               attrs: Union[List[str], Tuple[str, ...], Dict[str, Any]],
-               bases: Tuple[type, ...] = ...,
-               repr_ns: Optional[str] = ...,
-               repr: bool = ...,
-               cmp: bool = ...,
-               hash: Optional[bool] = ...,
-               init: bool = ...,
-               slots: bool = ...,
-               frozen: bool = ...,
-               str: bool = ...,
-               auto_attribs: bool = ...) -> type: ...
+def make_class(
+    name: str,
+    attrs: Union[List[str], Tuple[str, ...], Dict[str, Any]],
+    bases: Tuple[type, ...] = ...,
+    repr_ns: Optional[str] = ...,
+    repr: bool = ...,
+    cmp: bool = ...,
+    hash: Optional[bool] = ...,
+    init: bool = ...,
+    slots: bool = ...,
+    frozen: bool = ...,
+    str: bool = ...,
+    auto_attribs: bool = ...,
+) -> type: ...
 
 # _funcs --
 
@@ -181,17 +208,22 @@ def make_class(name: str,
 # FIXME: asdict/astuple do not honor their factory args.  waiting on one of these:
 # https://github.com/python/mypy/issues/4236
 # https://github.com/python/typing/issues/253
-def asdict(inst: Any,
-           recurse: bool = ...,
-           filter: Optional[_FilterType] = ...,
-           dict_factory: Type[Mapping[Any, Any]] = ...,
-           retain_collection_types: bool = ...) -> Dict[str, Any]: ...
+def asdict(
+    inst: Any,
+    recurse: bool = ...,
+    filter: Optional[_FilterType] = ...,
+    dict_factory: Type[Mapping[Any, Any]] = ...,
+    retain_collection_types: bool = ...,
+) -> Dict[str, Any]: ...
+
 # TODO: add support for returning NamedTuple from the mypy plugin
-def astuple(inst: Any,
-            recurse: bool = ...,
-            filter: Optional[_FilterType] = ...,
-            tuple_factory: Type[Sequence] = ...,
-            retain_collection_types: bool = ...) -> Tuple[Any, ...]: ...
+def astuple(
+    inst: Any,
+    recurse: bool = ...,
+    filter: Optional[_FilterType] = ...,
+    tuple_factory: Type[Sequence] = ...,
+    retain_collection_types: bool = ...,
+) -> Tuple[Any, ...]: ...
 def has(cls: type) -> bool: ...
 def assoc(inst: _T, **changes: Any) -> _T: ...
 def evolve(inst: _T, **changes: Any) -> _T: ...
@@ -200,7 +232,6 @@ def evolve(inst: _T, **changes: Any) -> _T: ...
 
 def set_run_validators(run: bool) -> None: ...
 def get_run_validators() -> bool: ...
-
 
 # aliases --
 
--- tests/typing_example.py
+++ tests/typing_example.py
@@ -1,4 +1,4 @@
-from typing import *
+from typing import Any, List
 
 import attr
 
