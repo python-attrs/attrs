e42756e14f71b5c015511e84a0e8ab8819d05ca1
mattsb42-aws <bullocm@amazon.com>
2018-08-15 23:12:42 -0700

    add tests for deep_iterable

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -12,7 +12,7 @@ import attr
 from attr import has
 from attr import validators as validator_module
 from attr._compat import TYPE
-from attr.validators import and_, in_, instance_of, optional, provides, is_callable
+from attr.validators import and_, in_, instance_of, optional, provides, deep_iterable, is_callable
 
 from .utils import simple_attr
 
@@ -265,6 +265,88 @@ class TestIn_(object):
         """
         v = in_([3, 4, 5])
         assert (("<in_ validator with options [3, 4, 5]>")) == repr(v)
+
+
+class TestDeepIterable(object):
+    """
+    Tests for `deep_iterable`.
+    """
+
+    def test_success_member_only(self):
+        """
+        If the member validator succeeds and the iterable validator is not set, nothing happens.
+        """
+        member_validator = instance_of(int)
+        v = deep_iterable(member_validator)
+        a = simple_attr("test")
+        v(None, a, [42])
+
+    def test_success_member_and_iterable(self):
+        """
+        If both the member and iterable validators succeed, nothing happens.
+        """
+        member_validator = instance_of(int)
+        iterable_validator = instance_of(list)
+        v = deep_iterable(member_validator, iterable_validator)
+        a = simple_attr("test")
+        v(None, a, [42])
+
+    def test_fail_invalid_member(self):
+        """
+        Raise member validator error if an invalid member is found.
+        """
+        member_validator = instance_of(int)
+        v = deep_iterable(member_validator)
+        a = simple_attr("test")
+        with pytest.raises(TypeError) as e:
+            v(None, a, [42, "42"])
+
+    def test_fail_invalid_iterable(self):
+        """
+        Raise iterable validator error if an invalid iterable is found.
+        """
+        member_validator = instance_of(int)
+        iterable_validator = instance_of(tuple)
+        v = deep_iterable(member_validator, iterable_validator)
+        a = simple_attr("test")
+        with pytest.raises(TypeError) as e:
+            v(None, a, [42])
+
+    def test_fail_invalid_member_and_iterable(self):
+        """
+        Raise iterable validator error if both the iterable and a member are invalid.
+        """
+        member_validator = instance_of(int)
+        iterable_validator = instance_of(tuple)
+        v = deep_iterable(member_validator, iterable_validator)
+        a = simple_attr("test")
+        with pytest.raises(TypeError) as e:
+            v(None, a, [42, "42"])
+
+    def test_repr_member_only(self):
+        """
+        Returned validator has a useful `__repr__` when only member validator is set.
+        """
+        member_validator = instance_of(int)
+        member_repr = "<instance_of validator for type <{type} 'int'>>".format(type=TYPE)
+        v = deep_iterable(member_validator)
+        expected_repr = "<deep_iterable validator for iterables of {member_repr}>".format(member_repr=member_repr)
+        assert ((expected_repr)) == repr(v)
+
+    def test_repr_member_and_iterable(self):
+        """
+        Returned validator has a useful `__repr__` when both member and iterable validators are set.
+        """
+        member_validator = instance_of(int)
+        member_repr = "<instance_of validator for type <{type} 'int'>>".format(type=TYPE)
+        iterable_validator = instance_of(list)
+        iterable_repr = "<instance_of validator for type <{type} 'list'>>".format(type=TYPE)
+        v = deep_iterable(member_validator, iterable_validator)
+        expected_repr = "<deep_iterable validator for {iterable_repr} iterables of {member_repr}>".format(
+            iterable_repr=iterable_repr,
+            member_repr=member_repr,
+        )
+        assert expected_repr == repr(v)
 
 
 class TestIsCallable(object):
