5b1b45efbdbf3e48bc2b001c55881bcd89765e07
Ryan Gabbard <gabbard@isi.edu>
2018-08-18 16:35:19 -0400

    Add documentation for cache_hash

--- src/attr/_make.py
+++ src/attr/_make.py
@@ -843,6 +843,11 @@ def attrs(
             builder.make_unhashable()
 
         if init is True:
+            if cache_hash:
+                raise TypeError(
+                    "Invalid value for cache_hash.  To use hash caching,"
+                    " init must be True"
+                )
             builder.add_init()
 
         return builder.build_class()
