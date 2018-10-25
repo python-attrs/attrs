1a095d1620966091bd51c25acec23ab639f5aa83
William Jamir Silva <williamjamir@gmail.com>
2018-10-25 07:50:37 -0300

    Add python_requires in setup.py (#454)
    This PR adds `python_requires` into `setup.py` to prevent `pip` from installing `attrs` in unsupported python versions.
    
    I know that currently `attrs` supports a wide range of python versions but I think that it's always good to have some sanity check =)

--- setup.py
+++ setup.py
@@ -113,6 +113,7 @@ if __name__ == "__main__":
         long_description=LONG,
         packages=PACKAGES,
         package_dir={"": "src"},
+        python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
         zip_safe=False,
         classifiers=CLASSIFIERS,
         install_requires=INSTALL_REQUIRES,
