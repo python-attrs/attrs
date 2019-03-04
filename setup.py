34efa98e92dd54a6077e8ed635baf5f0b6e6aa6f
Hynek Schlawack <hs@ox.cx>
2019-03-04 20:25:26 +0100

    add long_description_content_type
    fixes `twine check` warning

--- setup.py
+++ setup.py
@@ -111,6 +111,7 @@ if __name__ == "__main__":
         maintainer_email=find_meta("email"),
         keywords=KEYWORDS,
         long_description=LONG,
+        long_description_content_type="text/x-rst",
         packages=PACKAGES,
         package_dir={"": "src"},
         python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
