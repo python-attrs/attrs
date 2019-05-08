729681b5a888ad1bc9c49db5487171324f9e5be0
Hynek Schlawack <hs@ox.cx>
2019-05-08 16:13:55 -0400

    [WIP] Set up CI with Azure Pipelines (#510)
    * Set up CI with Azure Pipelines
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update setup.py
    
    * Update tox.ini
    
    * Update setup.py
    
    * Update azure-pipelines.yml
    
    * Update setup.py
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Delete .travis.yml
    
    * Update README.rst
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml

--- setup.py
+++ setup.py
@@ -49,6 +49,9 @@ EXTRAS_REQUIRE = {
 EXTRAS_REQUIRE["dev"] = (
     EXTRAS_REQUIRE["tests"] + EXTRAS_REQUIRE["docs"] + ["pre-commit"]
 )
+EXTRAS_REQUIRE["azure-pipelines"] = EXTRAS_REQUIRE["tests"] + [
+    "pytest-azurepipelines"
+]
 
 ###############################################################################
 
