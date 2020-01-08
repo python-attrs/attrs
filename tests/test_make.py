fa5b63c03c43db92f4c0df9afc7715050d65b684
Paul Ganssle <paul@ganssle.io>
2020-01-08 12:00:38 -0500

    Fix incompatibility between frozen and cache_hash
    Fixes GH issue #611: serialization of a class with frozen=True and
    cache_hash=True will now succeed rather than raising a
    FrozenInstanceError.

--- tests/test_make.py
+++ tests/test_make.py
@@ -1466,7 +1466,6 @@ class TestClassBuilder(object):
 
         assert [C2] == C.__subclasses__()
 
-    @pytest.mark.xfail(raises=attr.exceptions.FrozenInstanceError)
     def test_cache_hash_with_frozen_serializes(self):
         """
         Frozen classes with cache_hash should be serializable.
