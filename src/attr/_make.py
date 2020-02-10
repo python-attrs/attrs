d6a65fb89f8ac7868213f5cecdbec5b3611bfdca
Paul Ganssle <paul@ganssle.io>
2020-02-10 11:16:07 -0500

    Use a wrapper class for cache hash to prevent cache serialization (#620)
    * Use an self-clearing subclass to store hash cache
    
    Rather than attempting to remove the hash cache from the object state on
    deserialization or serialization, instead we store the hash cache in an
    object that reduces to None, thus clearing itself when pickled or
    copied.
    
    This fixes GH #494 and #613.
    
    Co-authored-by: Matt Wozniski <godlygeek@gmail.com>
    
    * Add test for two-argument __reduce__
    
    I couldn't think of any way to make a useful and meaningful class that
    has no state and also has no custom __reduce__ method, so I went
    minimalist with it.
    
    * Improve test for hash clearing behavior.
    
    Previously, there was some miniscule risk of hash collision, and also it
    was relying on the implementation details of `pickle` (the assumption
    that `hash()` is never called as part of `pickle.loads`).
    
    * Add improved testing around cache_hash
    
    * Update src/attr/_make.py
    
    Co-Authored-By: Ryan Gabbard <gabbard@isi.edu>
    
    * Update comment in slots_setstate
    
    Since the cached hash value is not actually serialized in __getstate__,
    __setstate__ is not actually "clearing" it on deserialization - it's
    initializing the value to None.
    
    * Add changelog entry
    
    * Remove changelog for #611
    
    This change was overshadowed by a more fundamental change in #620.
    
    Co-authored-by: Matt Wozniski <godlygeek@gmail.com>
    Co-authored-by: Ryan Gabbard <gabbard@isi.edu>

--- src/attr/_make.py
+++ src/attr/_make.py
@@ -70,6 +70,31 @@ Sentinel to indicate the lack of a value when ``None`` is ambiguous.
 """
 
 
+class _CacheHashWrapper(int):
+    """
+    An integer subclass that pickles / copies as None
+
+    This is used for non-slots classes with ``cache_hash=True``, to avoid
+    serializing a potentially (even likely) invalid hash value. Since ``None``
+    is the default value for uncalculated hashes, whenever this is copied,
+    the copy's value for the hash should automatically reset.
+
+    See GH #613 for more details.
+    """
+
+    if PY2:
+        # For some reason `type(None)` isn't callable in Python 2, but we don't
+        # actually need a constructor for None objects, we just need any
+        # available function that returns None.
+        def __reduce__(self, _none_constructor=getattr, _args=(0, "", None)):
+            return _none_constructor, _args
+
+    else:
+
+        def __reduce__(self, _none_constructor=type(None), _args=()):
+            return _none_constructor, _args
+
+
 def attrib(
     default=NOTHING,
     validator=None,
@@ -523,34 +548,6 @@ class _ClassBuilder(object):
         for name, value in self._cls_dict.items():
             setattr(cls, name, value)
 
-        # Attach __setstate__. This is necessary to clear the hash code
-        # cache on deserialization. See issue
-        # https://github.com/python-attrs/attrs/issues/482 .
-        # Note that this code only handles setstate for dict classes.
-        # For slotted classes, see similar code in _create_slots_class .
-        if self._cache_hash:
-            existing_set_state_method = getattr(cls, "__setstate__", None)
-            if existing_set_state_method:
-                raise NotImplementedError(
-                    "Currently you cannot use hash caching if "
-                    "you specify your own __setstate__ method."
-                    "See https://github.com/python-attrs/attrs/issues/494 ."
-                )
-
-            # Clears the cached hash state on serialization; for frozen
-            # classes we need to bypass the class's setattr method.
-            if self._frozen:
-
-                def cache_hash_set_state(chss_self, _):
-                    object.__setattr__(chss_self, _hash_cache_field, None)
-
-            else:
-
-                def cache_hash_set_state(chss_self, _):
-                    setattr(chss_self, _hash_cache_field, None)
-
-            cls.__setstate__ = cache_hash_set_state
-
         return cls
 
     def _create_slots_class(self):
@@ -612,11 +609,10 @@ class _ClassBuilder(object):
             __bound_setattr = _obj_setattr.__get__(self, Attribute)
             for name, value in zip(state_attr_names, state):
                 __bound_setattr(name, value)
-            # Clearing the hash code cache on deserialization is needed
-            # because hash codes can change from run to run. See issue
-            # https://github.com/python-attrs/attrs/issues/482 .
-            # Note that this code only handles setstate for slotted classes.
-            # For dict classes, see similar code in _patch_original_class .
+
+            # The hash code cache is not included when the object is
+            # serialized, but it still needs to be initialized to None to
+            # indicate that the first call to __hash__ should be a cache miss.
             if hash_caching_enabled:
                 __bound_setattr(_hash_cache_field, None)
 
@@ -1103,7 +1099,23 @@ def _make_hash(cls, attrs, frozen, cache_hash):
     unique_filename = _generate_unique_filename(cls, "hash")
     type_hash = hash(unique_filename)
 
-    method_lines = ["def __hash__(self):"]
+    hash_def = "def __hash__(self"
+    hash_func = "hash(("
+    closing_braces = "))"
+    if not cache_hash:
+        hash_def += "):"
+    else:
+        if not PY2:
+            hash_def += ", *"
+
+        hash_def += (
+            ", _cache_wrapper="
+            + "__import__('attr._make')._make._CacheHashWrapper):"
+        )
+        hash_func = "_cache_wrapper(" + hash_func
+        closing_braces += ")"
+
+    method_lines = [hash_def]
 
     def append_hash_computation_lines(prefix, indent):
         """
@@ -1111,14 +1123,18 @@ def _make_hash(cls, attrs, frozen, cache_hash):
         Below this will either be returned directly or used to compute
         a value which is then cached, depending on the value of cache_hash
         """
+
         method_lines.extend(
-            [indent + prefix + "hash((", indent + "        %d," % (type_hash,)]
+            [
+                indent + prefix + hash_func,
+                indent + "        %d," % (type_hash,),
+            ]
         )
 
         for a in attrs:
             method_lines.append(indent + "        self.%s," % a.name)
 
-        method_lines.append(indent + "    ))")
+        method_lines.append(indent + "    " + closing_braces)
 
     if cache_hash:
         method_lines.append(tab + "if self.%s is None:" % _hash_cache_field)
