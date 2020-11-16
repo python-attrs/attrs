32fb12789e5cba4b2e71c09e47196b10763ddd7d
Hynek Schlawack <hs@ox.cx>
2020-11-16 07:52:35 +0100

    Add white logo for dark mode
    Run both through svgo (saved 17.2% for white and 23% for black).

--- docs/conf.py
+++ docs/conf.py
@@ -99,11 +99,10 @@ pygments_style = "sphinx"
 html_theme = "furo"
 html_theme_options = {
     "sidebar_hide_name": True,
+    "light_logo": "attrs_logo.svg",
+    "dark_logo": "attrs_logo_white.svg",
 }
 
-# The name of an image file (relative to this directory) to place at the top
-# of the sidebar.
-html_logo = "_static/attrs_logo.svg"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
