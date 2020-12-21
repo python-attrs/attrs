f132c07e556dde35d5f7ef30b736800eb08e9f3b
Hynek Schlawack <hs@ox.cx>
2020-12-21 06:39:58 +0100

    Make NOTHING falsey (#732)
    * Make NOTHING falsey
    
    Fixes #720
    
    * Add newsfragment
    
    * Python 2

--- src/attr/_make.py
+++ src/attr/_make.py
@@ -69,6 +69,12 @@ class _Nothing(object):
     def __repr__(self):
         return "NOTHING"
 
+    def __bool__(self):
+        return False
+
+    def __len__(self):
+        return 0  # __bool__ for Python 2
+
 
 NOTHING = _Nothing()
 """
