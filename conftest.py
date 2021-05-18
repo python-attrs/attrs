461d619a43f0fa1f544f838e3d592ba761ef645d
Hynek Schlawack <hs@ox.cx>
2021-05-18 09:45:34 +0200

    Run Python 3.10 under coverage
    Signed-off-by: Hynek Schlawack <hs@ox.cx>

--- conftest.py
+++ conftest.py
@@ -4,6 +4,8 @@ import sys
 
 from hypothesis import HealthCheck, settings
 
+from attr._compat import PY310
+
 
 def pytest_configure(config):
     # HealthCheck.too_slow causes more trouble than good -- especially in CIs.
@@ -23,9 +25,9 @@ if sys.version_info[:2] < (3, 6):
             "tests/test_next_gen.py",
         ]
     )
-if sys.version_info[:2] < (3, 10):
+if not PY310:
     collect_ignore.extend(["tests/test_pattern_matching.py"])
-if sys.version_info[:2] >= (3, 10):
+if PY310:
     collect_ignore.extend(
         [
             "tests/test_mypy.yml",
