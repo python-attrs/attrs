dcf16b217f5ff810967d17880cc0c50e41dd2360
Jasper Spaans <j@jasper.es>
2021-10-03 19:44:20 +0200

    Please the linters.

--- tests/attr_import_star.py
+++ tests/attr_import_star.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import
 
+from attr import *  # noqa: F401,F403
+
+
 # This is imported by test_import::test_import_attr_star; this must
 # be done indirectly because importing * is only allowed on module level,
 # so can't be done inside a test.
-
-from attr import *  # noqa: F403
