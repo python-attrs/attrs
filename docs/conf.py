7c44cfb3c196f547d351af44e411b43f65819714
Hynek Schlawack <hs@ox.cx>
2023-04-26 18:31:29 -0600

    Fix intersphinx (#1130)

--- docs/conf.py
+++ docs/conf.py
@@ -159,9 +159,7 @@ texinfo_documents = [
 
 epub_description = "Python Clases Without Boilerplate"
 
-intersphinx_mapping = {
-    "https://docs.python.org/3": None,
-}
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
 
 # Allow non-local URIs so we can have images in CHANGELOG etc.
 suppress_warnings = ["image.nonlocal_uri"]
