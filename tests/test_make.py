28379a4fa648d1e9ed08d9da2d057a7a3ca92e06
Danny Cooper <diabolo.dan@gmail.com>
2024-01-03 21:16:32 +0000

    fixup py 3.7 test

--- tests/test_make.py
+++ tests/test_make.py
@@ -21,7 +21,7 @@ from hypothesis.strategies import booleans, integers, lists, sampled_from, text
 import attr
 
 from attr import _config
-from attr._compat import PY310
+from attr._compat import PY310, PY_3_8_PLUS
 from attr._make import (
     Attribute,
     Factory,
@@ -1771,6 +1771,7 @@ class TestClassBuilder:
 
         assert [C2] == C.__subclasses__()
 
+    @pytest.mark.skipif(not PY_3_8_PLUS, reason="cached_property is 3.8+")
     def test_no_references_to_original_when_using_cached_property(self):
         """
         When subclassing a slotted class and using cached property, there are no stray references to the original class.
