1fd3af0b948da235c5bfa912ddfcc8cf86cd9416
Hynek Schlawack <hs@ox.cx>
2024-07-18 11:00:27 +0200

    Write specific benchmarks for CodSpeed (#1306)
    * Write specific benchmarks for CodSpeed
    
    * Add frozen benchmark
    
    * Remove instantiation noise
    
    * Put into separate dir like cattrs
    
    * Be consistent

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -111,7 +111,6 @@ class TestFunctional:
     Functional tests.
     """
 
-    @pytest.mark.benchmark()
     @pytest.mark.parametrize("cls", [C2, C2Slots])
     def test_fields(self, cls):
         """
@@ -146,7 +145,6 @@ class TestFunctional:
             ),
         ) == attr.fields(cls)
 
-    @pytest.mark.benchmark()
     @pytest.mark.parametrize("cls", [C1, C1Slots])
     def test_asdict(self, cls):
         """
@@ -182,7 +180,6 @@ class TestFunctional:
 
         assert "C3(_x=1)" == repr(C3(x=1))
 
-    @pytest.mark.benchmark()
     @given(booleans(), booleans())
     def test_programmatic(self, slots, frozen):
         """
@@ -340,7 +337,6 @@ class TestFunctional:
         """
         assert Meta is type(cls)
 
-    @pytest.mark.benchmark()
     def test_default_decorator(self):
         """
         Default decorator sets the default and the respective method gets
@@ -544,7 +540,6 @@ class TestFunctional:
         assert "itemgetter" == attr.fields(C).itemgetter.name
         assert "x" == attr.fields(C).x.name
 
-    @pytest.mark.benchmark()
     def test_auto_exc(self, slots, frozen):
         """
         Classes with auto_exc=True have a Exception-style __str__, compare and
@@ -739,7 +734,6 @@ class TestFunctional:
         assert "_setattr('y', y)" in src
         assert object.__setattr__ != D.__setattr__
 
-    @pytest.mark.benchmark()
     def test_unsafe_hash(self, slots):
         """
         attr.s(unsafe_hash=True) makes a class hashable.
