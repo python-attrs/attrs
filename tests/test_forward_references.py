074f64d5598317b3ef9ff46421b966e7d6ae87ae
Tin Tvrtković <tinchester@gmail.com>
2025-07-29 22:51:45 +0200

    Add test

---
+++ tests/test_forward_references.py
@@ -0,0 +1,14 @@
+"""Tests for behavior specific to forward references via PEP 749."""
+
+from attrs import define
+
+
+def test_forward_class_reference():
+    """Class A should be able to reference B even though it is defined later."""
+
+    @define
+    class A:
+        b: B
+
+    class B:
+        pass
