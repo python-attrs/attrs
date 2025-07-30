5a9fdf53bf591961949255eea989ccef10d2edaa
Hynek Schlawack <hs@ox.cx>
2025-07-30 11:03:04 +0200

    Apply docstring standard

--- tests/test_forward_references.py
+++ tests/test_forward_references.py
@@ -1,10 +1,14 @@
-"""Tests for behavior specific to forward references via PEP 749."""
+"""
+Tests for behavior specific to forward references via PEP 749.
+"""
 
 from attrs import define
 
 
 def test_forward_class_reference():
-    """Class A should be able to reference B even though it is defined later."""
+    """
+    Class A can reference B even though it is defined later.
+    """
 
     @define
     class A:
