d1af8417127b49b03f2ea1d46b61a65be305ec82
Pieter Eendebak <pieter.eendebak@gmail.com>
2025-08-29 17:36:26 +0200

    coverage

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -66,6 +66,15 @@ class TestAsDict:
             C(C(1, 2), C(3, 4)), dict_factory=dict_class
         )
 
+    def test_non_atomic_types(self, C):
+        class Int(int):
+            pass
+
+        c = C(Int(10), [Int(1), 2])
+        expected = {'x': 10, 'y': [1, 2]}
+        assert expected == asdict(c)
+        assert type(asdict(c)['x']) is Int
+
     def test_nested_lists(self, C):
         """
         Test unstructuring deeply nested lists.
