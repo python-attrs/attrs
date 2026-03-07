a1f6d3a2d4132f8d7b7c100a2afdba981c48c3ec
Zachary Spector <public@zacharyspector.com>
2026-03-08 11:35:10 +1300

    Add type hints to `sphinx_cached_property.py`

--- src/attrs/sphinx_cached_property.py
+++ src/attrs/sphinx_cached_property.py
@@ -6,6 +6,8 @@ conf.py to use this. Otherwise, cached properties of ``@define(slots=True)``
 classes will be inaccessible.
 
 """
+from functools import cached_property
+from typing import Any
 
 from sphinx.application import Sphinx
 
@@ -14,7 +16,7 @@ from . import __version__
 
 def get_cached_property_for_member_descriptor(
     cls: type, name: str, default=None
-):
+) -> cached_property | Any:
     """If the attribute is for a cached property, return the ``cached_property``
 
     Otherwise, delegate to normal ``getattr``
@@ -26,7 +28,7 @@ def get_cached_property_for_member_descriptor(
     return props[name]
 
 
-def setup(app: Sphinx):
+def setup(app: Sphinx) -> dict[str, str | bool]:
     """Install the special attribute getter for cached properties of slotted classes"""
     app.add_autodoc_attrgetter(
         object, get_cached_property_for_member_descriptor
