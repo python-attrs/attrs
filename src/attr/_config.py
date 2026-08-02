f38b8a3f1625060aa4245930822c34c11c252f83
Hynek Schlawack <hs@ox.cx>
2026-08-02 12:21:33 +0200

    docs: format notice

--- src/attr/_config.py
+++ src/attr/_config.py
@@ -9,9 +9,9 @@ def set_run_validators(run):
     """
     Set whether or not validators are run.  By default, they are run.
 
-    .. deprecated:: 21.3.0 It will not be removed, but it also will not be
-        moved to new ``attrs`` namespace. Use `attrs.validators.set_disabled()`
-        instead.
+    .. deprecated:: 21.3.0
+       It will not be removed, but it also will not be moved to new ``attrs``
+       namespace. Use `attrs.validators.set_disabled()` instead.
     """
     if not isinstance(run, bool):
         msg = "'run' must be bool."
@@ -24,8 +24,8 @@ def get_run_validators():
     """
     Return whether or not validators are run.
 
-    .. deprecated:: 21.3.0 It will not be removed, but it also will not be
-        moved to new ``attrs`` namespace. Use `attrs.validators.get_disabled()`
-        instead.
+    .. deprecated:: 21.3.0
+        It will not be removed, but it also will not be moved to new ``attrs``
+        namespace. Use `attrs.validators.get_disabled()` instead.
     """
     return _run_validators
